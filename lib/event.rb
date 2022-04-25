8d1721a5b71ae4e74750e05bdb0184c88a015e06
Olivia <oliviarudolf67@gmail.com>
2022-04-25 19:27:19 +0200

    First commit

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,42 @@
+require 'pry'
+require 'time'
+
 class Event
+    attr_accessor :start_date, :duration
+
+    def initialize(start_date, duration, title, attendees)
+        @start_date = Time.parse(start_date)
+        @duration = duration.to_i
+        @title = title
+        @attendees = [attendees]
+    end
+
+    def postpone_24h
+        @start_date += (3600 * 24)
+    end
+
+    def end_date
+        end_date = @start_date + (@duration * 60)
+        return end_date.hour
+    end
+
+    def is_past?
+        Time.now > @start_date
+    end
+
+    def is_future?
+        !self.is_past? # ! est le contraire de ?
+    end
+
+    def to_s 
+        puts "Titre : #{@title}"
+        puts "Date de début : #{@start_date}"
+        puts "Durée : #{@duration}minutes"
+        puts "Invités : #{@attendees.join(", ")}" # .join permet de ne pas voir les [] de l'array et choisir comment séparer les handles
+    end
+
 
 end
+
+binding.pry
+puts "end of file"
--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,32 @@
+require "pry"
+
 class User
+    attr_accessor :email, :age
+    @@all_users = []
+
+    def initialize(email_to_save, age_to_save)
+        @email = email_to_save
+        @age = age_to_save
+        @@all_users << self
+        puts "Email and age saved !"  
+    end
+
+    def self.all
+        return @@all_users 
+    end
+
+    def self.find_by_email(email)
+        @@all_users.each do |user|
+            return user if user.email == email 
+        end
+    end
 
 end
+
+olivia = User.new("olivia@gmail.com", 24)
+louis = User.new("louis@gmail.com", 26)
+samu = User.new("samu@gmail.com", 25)
+james = User.new("james@gmail.com", 26)
+
+binding.pry
+puts "End of file"
